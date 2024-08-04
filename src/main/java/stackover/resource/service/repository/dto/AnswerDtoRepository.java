6fd75dcf61ecf36cd46a0bf4f134a65efa67cfd8
Aleksey <13Alexnik@gmail.com>
2024-08-04 15:29:10 +0300

    Revert "(feature-issue-35): Добавление логина"
    This reverts commit b9be350bb12d4633d9809eebf26d91bf9e64589b.

--- src/main/java/stackover/resource/service/repository/dto/AnswerDtoRepository.java
+++ src/main/java/stackover/resource/service/repository/dto/AnswerDtoRepository.java
@@ -10,7 +10,23 @@ import java.util.List;
 @Repository
 public interface AnswerDtoRepository extends JpaRepository<AnswerResponseDto, Long> {
 
-    @Query("select p from Post p where p.user.id=:id and p.title like :title")
+    @Query("""
+                SELECT new stackover.resource.service.service.dto.response.AnswerResponseDto (
+                    Answer.id,
+                    Answer.account_id,
+                    Answer.question_id,
+                    Answer.html_body,
+                    Answer.persist_date,
+                    Answer.is_helpful,
+                    Answer.date_accept_time,
+                    (SELECT SUM(CAST(Votes_on_answers.vote_type_answer AS bigint)) AS count_valuable FROM Votes_on_answers JOIN Answer ON Votes_on_answers.answer_id = Answer.id),
+                    (SELECT SUM(Reputation.count) AS count_user_reputation FROM Reputation JOIN Answer ON Reputation.answer_id = Answer.id),
+                    (SELECT User_entity.image_link FROM User_entity JOIN Answer ON User_entity.id = Answer.account_id),
+                    (SELECT User_entity.nickname FROM User_entity JOIN Answer ON User_entity.id = Answer.account_id),
+                    (SELECT COUNT(Votes_on_answers.id) AS countVote FROM Votes_on_answers JOIN Answer ON Votes_on_answers.answer_id = Answer.id),
+                    (SELECT Votes_on_answers.vote_type_answer FROM Votes_on_answers JOIN Answer ON Votes_on_answers.answer_id = Answer.id JOIN User_entity ON User_entity.id = Votes_on_answers.user_id WHERE User_entity.id = :accountId)
+                ) FROM Answer WHERE Answer.question_id = :questionId
+                """)
     List<AnswerResponseDto> getAnswersDtoByQuestionId(@Param("questionId") Long questionId, @Param("accountId") Long accountId);
 
 }
